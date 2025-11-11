f1e7fed697dca68586919aaa0d9bc75583475ae4
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-11-11 09:24:28 +0100

    built the top politician component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./components/hero";
+import TopPoliticians from "./components/topPoliticians";
 
 const Page = () => {
   return (
     <div>
       <Hero />
+      <TopPoliticians />
     </div>
   );
 };
