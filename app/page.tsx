f2b828864684a031ecfc7d74ddf7fdc107d6ab3d
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-11-19 20:37:36 +0100

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Hero from "./components/hero";
 import TopPoliticians from "./components/topPoliticians";
-import Partners from "./components/partners";
+//import Partners from "./components/partners";
 import FeaturedResources from "./components/featuredResources";
 
 const Page = () => {
   return (
     <div>
       <Hero />
+      {/*
       <Partners />
+      */}
       <TopPoliticians />
       <FeaturedResources />
     </div>
