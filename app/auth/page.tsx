d5cc644f8100e2ddedf9c368d4ebe6f936f109da
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2025-12-02 10:16:43 +0100

    feat: add Google OAuth login page with token handling and redirection

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -60,7 +60,7 @@ const GoogleLoginButton = () => {
 
 const LoginPage: React.FC = () => {
   return (
-    <GoogleOAuthProvider clientId="904187096076-pr1acp7ricmtc2t3cdap19fi1q6j5nc7.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="649644210746-pripag64sp9svnrumn4oto05eo9le8bp.apps.googleusercontent.com">
       <div className="flex relative flex-col items-center justify-center fontroboto min-h-screen bg-gray-50 p-4 w-full">
         <div className="absolute top-6 left-6">
           <Link href="/auth" className="flex items-center gap-2 font-mont">
